4275a49b23364bc7ed99514673c783ce4e3491de
Daniel Haycraft <dhcopy1@gmail.com>
2022-11-03 20:08:06 -0700

    yas

--- server/server.js
+++ server/server.js
@@ -17,22 +17,28 @@ var rollbar = new Rollbar({
 // record a generic message and send it to Rollbar
 rollbar.log('Hello world!')
 
-app.use(express.static(path.join(__dirname, "../public")));
+// app.use(express.static(path.join(__dirname, '/public')))
+// app.use('/js', express.static(path.join(__dirname, 'public/main.js')))
+// app.use('/styles', express.static(path.join(__dirname, 'public/main.css')))
+// app.use("/" ,express.static(path.join(__dirname, "/public")));
 // app.use("/public", express.static(path.join(__dirname, "../public")));
 
 const buddies = require('./db.json')
-app.get('/api/buddies', (req, res) => {
+
+app.get('api/buddies', (req, res) => {
     rollbar.info('working .(get)')
     res.status(200).send(buddies)
 })
-
-app.delete('/api/buddies/:index', (req, res) => {
+app.get('api/test', (req, res) =>{
+    res.sendStatus(418)
+})
+app.delete('api/buddies/:index', (req, res) => {
     const deleteIndex = +req.params.index
     buddies.splice(deleteIndex, 1)
     rollbar.info('termiated fluffer')
     res.status(200).send(buddies)
 })
-app.post('/api/buddies', (req, res)=> {
+app.post('api/buddies', (req, res)=> {
     const { dogName } = req.body;
     buddies.push({
         dogName, 
@@ -44,7 +50,7 @@ app.post('/api/buddies', (req, res)=> {
         if (index ===  -1 && dogName !== ''){
             rollbar.log('fullys added successfully')
             res.status(200).send(buddies)
-        } else if (na === ''){
+        } else if (dogName === ''){
             rollbar.error("no name provided man!")
             res.status(400).send('you need to put in a name bruv')
         } else {
@@ -57,4 +63,4 @@ app.post('/api/buddies', (req, res)=> {
 })
 const port = process.env.PORT || 5050
 // hey matt!
-app.listen(port, () => console.log(`Server is parting on port ${port} till it breaks`))
+app.listen(port, () => console.log(`Server is partying on port ${port} till it breaks`))
